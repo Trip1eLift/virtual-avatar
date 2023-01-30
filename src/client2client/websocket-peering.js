c2dbbfa91ec212c18678fb1ae75c8e3b9c02d266
Trip1eLift <josephjbc@gmail.com>
2023-01-30 01:20:18 +0000

    support remote streaming on prod

--- src/client2client/websocket-peering.js
+++ src/client2client/websocket-peering.js
@@ -16,7 +16,7 @@ const ICE_config = {
       'username': '28224511:1379330808'
     }
   ],
-  iceCandidatePoolSize: 10,
+  iceCandidatePoolSize: 30,
 };
 
 const Peer_options = {
@@ -37,6 +37,9 @@ const Peer_options = {
  * { message_type: 'answer-peer-connection',  message: answer    } answer = await peerConnection.createAnswer();
  * { message_type: 'new-ice-candidate',       message: candidate }
  */
+
+// TODO: handle cross device peer connection
+
 const MESSAGE_TYPE = {
   push:                 'push',
   startPeerConnection:  'start-peer-connection',
@@ -74,14 +77,18 @@ class WebSocketPeering {
 
     const datachannel = peer.createDataChannel("data");
     
+    // 7. Peer connection established
     datachannel.onopen = () => {
       console.log("[peer] Connection established; Closing websocket");
       this.socket.close(1000, "websocket is no longer needed.");
       this.dc_open = true;
+
+      console.log("7. Peer connection established");
     };
     
     datachannel.onclose = () => {
       console.log("[peer] Connection close.");
+      this.dc_open = false;
     };
 
     this.socket = undefined;
@@ -118,19 +125,36 @@ class WebSocketPeering {
         await peer.setLocalDescription(offer);
 
         socket.send(JSON.stringify({message_type: MESSAGE_TYPE.offerPeerConnection, message: offer}));
+        //console.log("2. Owner reacts to start-peer-connection");
       }
 
       // 4. Owner recieves answer data
       if (payload.message_type === MESSAGE_TYPE.answerPeerConnection) {
         const remoteDesc = new RTCSessionDescription(payload.message);
         await peer.setRemoteDescription(remoteDesc);
+        //console.log("4. Owner recieves answer data");
+      }
+
+      // 6.5 Owner recieves a remote ICE candidate
+      if (payload.message_type === MESSAGE_TYPE.newIceCandidate) {
+        if (payload.message) {
+          try {
+            console.log(payload.message.candidate); // for debug
+            await peer.addIceCandidate(payload.message);
+          } catch (e) {
+            console.error('Error adding received ice candidate', e);
+          }
+        }
+        //console.log("6.5 Owner recieves a remote ICE candidate")
       }
     }
 
     // 5. Owner retrieves a local ICE candidate
     peer.addEventListener('icecandidate', event => {
       if (event.candidate) {
+        console.log(event.candidate.candidate); // for debug
         socket.send(JSON.stringify({message_type: MESSAGE_TYPE.newIceCandidate, message: event.candidate}));
+        //console.log("5. Owner retrieves a local ICE candidate");
       }
     });
     
@@ -149,13 +173,14 @@ class WebSocketPeering {
 
       // 1. Guest triggers start-peer-connection
       socket.send(JSON.stringify({message_type: MESSAGE_TYPE.startPeerConnection}));
+      //console.log("1. Guest triggers start-peer-connection");
     };
   
     socket.onmessage = async function(event) {
       const payload = JSON.parse(event.data);
 
       if (payload.message_type === MESSAGE_TYPE.push) {
-        console.log(`[guest socket] recieved: ${payload.message}`);
+        //console.log(`[guest socket] recieved: ${payload.message}`);
       }
 
       // 3. Guest recieves offer data
@@ -165,20 +190,31 @@ class WebSocketPeering {
         await peer.setLocalDescription(answer);
 
         socket.send(JSON.stringify({message_type: MESSAGE_TYPE.answerPeerConnection, message: answer}));
+        //console.log("3. Guest recieves offer data")
       }
 
       // 6. Guest recieves a remote ICE candidate
       if (payload.message_type === MESSAGE_TYPE.newIceCandidate) {
         if (payload.message) {
           try {
-            // console.log(payload.message.candidate); // for debug
+            console.log(payload.message.candidate); // for debug
             await peer.addIceCandidate(payload.message);
           } catch (e) {
             console.error('Error adding received ice candidate', e);
           }
         }
+        //console.log("6. Guest recieves a remote ICE candidate")
       }
     };
+
+    // 5.5 Guest retrieves a local ICE candidate
+    peer.addEventListener('icecandidate', event => {
+      if (event.candidate) {
+        console.log(event.candidate.candidate); // for debug
+        socket.send(JSON.stringify({message_type: MESSAGE_TYPE.newIceCandidate, message: event.candidate}));
+        //console.log("5.5 Guest retrieves a local ICE candidate");
+      }
+    });
     
     attachCommonSocketHandlers(socket);
     this.socket = socket;
